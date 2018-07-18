abab391f6fa5b83206779da3a67fab512b5c10cc
Wout Borghgraef <wout.borghgraef@gmail.com>
2018-07-19 01:39:08 +0200

    Add basic autosuggest component

---
+++ src/Form.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import AutoSuggest from './AutoSuggest';
+
+class Form extends React.Component {
+  render() {
+  	return <AutoSuggest />;
+  }
+}
+
+export default Form;
