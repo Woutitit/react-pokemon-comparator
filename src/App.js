561c77664f4edbfd5fb006384ccb9fd304659850
Wout Borghgraef <wout.borghgraef@gmail.com>
2018-07-20 18:33:25 +0200

    Test calls to pokemon API

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // Obviously we should also supply so OUTSIDE the autosuggest component we should filter the list that should be supplied and shown by our autosuggest component.
   state = {
-    suggestions: ["apple", "banana", "orange"]
+    suggestions: []
   }
 
   getSuggestions = () => {
@@ -13,12 +14,17 @@ class App extends Component {
   }
 
   updateSuggestions = () => {
-
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=949').then((response) => {
+      const pokemon = response.data.results;
+      console.log(pokemon);
+      // Now, of this list we should check which pokemon names are relevant.
+      this.setState({suggestions: pokemon})
+    })
   }
 
   render() {
     const listItems = this.state.suggestions.map((item) => { 
-      return <li key={item}>{item}</li> 
+      return <li key={item.url}>{item.name}</li> 
     })
 
     return (
